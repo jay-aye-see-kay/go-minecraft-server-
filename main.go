6bf3ab27b16cc4a50ff513917eef4bb2471769f4
Jack Rose <user@jackrose.co.nz>
2022-08-18 19:32:52 +1000

    tidy up routes

--- main.go
+++ main.go
@@ -15,41 +15,37 @@ import (
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "<div>Pages:</div>")
-	io.WriteString(w, "<div><ul>")
-	io.WriteString(w, "<li><a href='/containers'>list of containers</a></li>")
-	io.WriteString(w, "<li><a href='/state-machine'>a graphviz representation of the state machine</a></li>")
-	io.WriteString(w, "</ul></div>")
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
 }
 
 func getStateMachineDiagram(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/state-machine.html"))
-
 	tmpl.Execute(w, mcss.MakeStateMachine().ToGraph())
 }
 
-func main() {
+func getContainers(w http.ResponseWriter, r *http.Request) {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
 	}
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, container := range containers {
+		img_name := fmt.Sprintf("%s %s\n", container.ID[:10], container.Image)
+		io.WriteString(w, img_name)
+	}
+}
 
+func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/state-machine", getStateMachineDiagram)
-	http.HandleFunc("/containers", func(w http.ResponseWriter, r *http.Request) {
-		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-		if err != nil {
-			panic(err)
-		}
-		for _, container := range containers {
-			img_name := fmt.Sprintf("%s %s\n", container.ID[:10], container.Image)
-			io.WriteString(w, img_name)
-		}
-	})
+	http.HandleFunc("/containers", getContainers)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
