b93b3315cccb92e7fcbdd03fd989a5d38f9fd2ca
Jack Rose <user@jackrose.co.nz>
2022-08-18 14:08:33 +1000

    play with docker client, return container list

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
+	io.WriteString(w, "<div>Pages:</div>")
+	io.WriteString(w, "<div><ul>")
+	io.WriteString(w, "<li><a href='/containers'>list of containers</a></li>")
+	io.WriteString(w, "</ul></div>")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
@@ -18,8 +25,22 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/containers", func(w http.ResponseWriter, r *http.Request) {
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+		if err != nil {
+			panic(err)
+		}
+		for _, container := range containers {
+			img_name := fmt.Sprintf("%s %s\n", container.ID[:10], container.Image)
+			io.WriteString(w, img_name)
+		}
+	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
